0b384cd0ad80daa37234fc6615ee63fab410472d
Abdi Yusuf <ayusufadan@gmail.com>
2022-07-27 18:24:19 -0500

    your message

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-res.sendFile(path.join(__dirname, '../index.html'))
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
+const app = express()
+
+
+// The index.html file has been moved into my public folder so /public needs to be added to the path string. 
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+// This is how we can connect the JS and CSS files so they can be accessed by Heroku.
 app.use(express.static(path.join(__dirname, '../public')))
 
 const port = process.env.PORT || 4005
